55c4cd3c27c0637c1ccb25f9b3245c6993067b48
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-02-17 08:32:21 -0800

    add working spec for rock beating scissors

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -1,6 +1,15 @@
 <?php
     class RockPaperScissors {
 
+        function playGame($player1, $player2)
+        {
+            $player1winner = "Player 1";
+            $player2winner = "Player 2";
+            $draw = "Draw";
 
+            if ($player1 == "rock" && $player2 == "scissors") {
+                return $player1winner;
+            }
+        }
     }
 ?>
