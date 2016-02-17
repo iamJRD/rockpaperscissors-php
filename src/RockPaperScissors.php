3f9ac0db2c9cb0624fd1c63a64e94a518e354175
jared beckler and torrence stratton <tlstratton@gmail.com>
2016-02-17 09:03:17 -0800

    add working specs for draw and scissors win

--- src/RockPaperScissors.php
+++ src/RockPaperScissors.php
@@ -7,9 +7,48 @@
             $player2winner = "Player 2";
             $draw = "Draw";
 
-            if ($player1 == "rock" && $player2 == "scissors") {
+            if ($player1 == $player2) {
+                return $draw;
+            }
+            else if ($player1 == "rock" && $player2 == "scissors") {
                 return $player1winner;
+            }
+            else if ($player2 == "rock" && $player1 == "scissors") {
+                return $player2winner;
+            }
+            else if ($player1 == "scissors" && $player2 == "paper") {
+                return $player1winner;
+            }
+            else if ($player2 == "scissors" && $player1 == "paper") {
+                return $player2winner;
+            }
+            else {
+                echo "You have a problem.";
             }
         }
     }
 ?>
+
+
+if (player1 == player2) {
+    return 3
+}
+
+
+
+
+if number = 3
+{
+    output = draw;
+}
+
+if player 1 equals "rock" && player 2 equals "scissors" {
+    return 1
+}
+    else { return 2
+}
+
+if number = return 1 {
+    output player 1
+}
+else output player 2
--- tests/RockPaperScissorsTest.php
+++ tests/RockPaperScissorsTest.php
@@ -3,6 +3,21 @@
 
     class RockPaperScissorsTest extends PHPUnit_Framework_TestCase {
 
+
+        function test_draw()
+        {
+            // Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $first_input = "paper";
+            $second_input = "paper";
+
+            // Act
+            $result = $test_RockPaperScissors->playGame($first_input, $second_input);
+
+            // Assert
+            $this->assertEquals("Draw", $result);
+        }
+        
         function test_rock_scissors()
         {
             //Arrange
@@ -16,6 +31,22 @@
             //Assert
             $this->assertEquals("Player 1", $result);
         }
+
+        function test_scissors_paper()
+        {
+            //Arrange
+            $test_RockPaperScissors = new RockPaperScissors;
+            $first_input = "paper";
+            $second_input = "scissors";
+
+            //Act
+            $result = $test_RockPaperScissors->playGame($first_input, $second_input);
+
+            //Assert
+            $this->assertEquals("Player 2", $result);
+        }
+
+
 
     }
 ?>
